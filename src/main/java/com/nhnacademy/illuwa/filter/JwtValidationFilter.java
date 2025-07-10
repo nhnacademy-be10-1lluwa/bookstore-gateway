4113a18d7b7095c5bc8366f301d9c4daed65a314
pear_c <zxcv9676@naver.com>
2025-07-10 16:13:16 +0900

    fix: WhiteList 수정

--- src/main/java/com/nhnacademy/illuwa/filter/JwtValidationFilter.java
+++ src/main/java/com/nhnacademy/illuwa/filter/JwtValidationFilter.java
@@ -32,7 +32,7 @@ public class JwtValidationFilter implements GlobalFilter {
 
     // 인증 제외(Path 화이트리스트)
     private static final List<String> WHITE_LIST = List.of(
-            "/auth", "/login", "/signup", "/static", "/actuator", "/books", "/order/guest/order-history"
+            "/api/auth", "/api/login", "/api/signup", "/static", "/actuator", "/api/books", "/api/order/guest/order-history"
     );
 
     @Override
