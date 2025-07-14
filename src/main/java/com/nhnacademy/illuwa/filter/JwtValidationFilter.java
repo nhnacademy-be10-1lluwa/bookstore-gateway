a3f2f5737fa64deeee19ff1a2979dc93b1d97569
pear_c <zxcv9676@naver.com>
2025-07-14 15:24:56 +0900

    feat: swagger url 삭제

--- src/main/java/com/nhnacademy/illuwa/filter/JwtValidationFilter.java
+++ src/main/java/com/nhnacademy/illuwa/filter/JwtValidationFilter.java
@@ -35,8 +35,7 @@ public class JwtValidationFilter implements GlobalFilter {
             "/api/auth", "/api/login", "/api/signup", "/static", "/actuator", "/api/books", "/api/order/guest/order-history",
             "/api/members/check-status",
             "/api/members/inactive/verification",
-            "/api/members/inactive/verification/verify",
-            "/swagger-ui/"
+            "/api/members/inactive/verification/verify"
     );
 
     @Override
