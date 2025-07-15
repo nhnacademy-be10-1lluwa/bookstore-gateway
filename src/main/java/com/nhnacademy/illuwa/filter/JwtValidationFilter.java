b7c793c7f3b3c92ab1b4a2fdce467df0a871b02d
CGE <bestofGE@naver.com>
2025-07-15 21:41:14 +0900

    feat: 화이트리스트 /api/guests 추가

--- src/main/java/com/nhnacademy/illuwa/filter/JwtValidationFilter.java
+++ src/main/java/com/nhnacademy/illuwa/filter/JwtValidationFilter.java
@@ -32,7 +32,7 @@ public class JwtValidationFilter implements GlobalFilter {
 
     // 인증 제외(Path 화이트리스트)
     private static final List<String> WHITE_LIST = List.of(
-            "/api/auth", "/api/login", "/api/signup", "/static", "/actuator", "/api/books", "/api/order/guest",
+            "/api/auth", "/api/login", "/api/signup", "/static", "/actuator", "/api/books", "/api/order/guest", "/api/guests",
             "/api/members/check-status",
             "/api/members/inactive/verification",
             "/api/members/inactive/verification/verify"
