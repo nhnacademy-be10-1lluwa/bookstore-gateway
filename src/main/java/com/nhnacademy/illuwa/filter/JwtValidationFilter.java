de2a5dd2eafe406a1152a1899ebefd229e24fd6d
pear_c <zxcv9676@naver.com>
2025-07-17 19:30:38 +0900

    feat: api 경로 수정

--- src/main/java/com/nhnacademy/illuwa/filter/JwtValidationFilter.java
+++ src/main/java/com/nhnacademy/illuwa/filter/JwtValidationFilter.java
@@ -34,7 +34,7 @@ public class JwtValidationFilter implements GlobalFilter {
 
     // 인증 제외(Path 화이트리스트)
     private static final List<String> WHITE_LIST = List.of(
-            "/api/auth", "/api/login", "/api/signup",
+            "/api/auth", "/api/login", "/api/signup", "/api/members/login",
             "/api/members/inactive", "/api/members/names", "/api/guests",
             "/api/books", "/api/reviews", "/api/search/category",
             "/api/order/guest",
